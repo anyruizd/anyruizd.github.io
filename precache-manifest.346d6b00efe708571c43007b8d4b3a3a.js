37a574b0788ef4ff14b0dc1b831d321b7b2f1126
anyruizd <anyruizd@gmail.com>
2020-06-15 02:39:44 -0500

    Updates

---
+++ precache-manifest.346d6b00efe708571c43007b8d4b3a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ca7cc724bba9224d6fdf0a90b33ee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce58b83ca4683158abcf",
+    "url": "/static/css/main.5044beac.chunk.css"
+  },
+  {
+    "revision": "3cca7aa24754f0765d65",
+    "url": "/static/js/2.6928bb32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6928bb32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce58b83ca4683158abcf",
+    "url": "/static/js/main.befb4df4.chunk.js"
+  },
+  {
+    "revision": "4378c44d91d952bfd3d4",
+    "url": "/static/js/runtime-main.7fc84c71.js"
+  }
+]);
