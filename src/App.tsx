bcb00e67fdb5fcefb54aceef2a28e3538869e324
anyruizd <any@skillshare.com>
2020-06-15 01:08:55 -0500

    refactor: ⚛️  add react initial config

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <p>
+          <span role="img" aria-label="sparkles">✨</span>
+          Any's site is under construction
+          <span role="img" aria-label="sparkles">✨</span>
+          </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
