6ffee79c5015b9cde8cb3c2661469004f6a3cf64
anyruizd <any@skillshare.com>
2020-06-15 01:08:55 -0500

    refactor: ⚛️  add react initial config

--- src/components/App/createApp.js
+++
@@ -1,7 +0,0 @@
-/**
- * Creates the application
- * @param {Object} node - HTML element
- */
-export function createApp (node) {
-  node.innerHTML = '<p> ✨ Any\'s site is under construction ✨</p>';
-}
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from './components/App/index.js';
-
-const rootElement = document.querySelector('#root');
-createApp(rootElement);
