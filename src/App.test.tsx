eb08e5bed39e40fcc743ebc3a6895202a4699d39
anyruizd <anyruizd@gmail.com>
2020-07-05 01:13:08 -0500

    🔧 Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("renders any's site warning", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/Any's site is under construction/i);
+  expect(textElement).toBeInTheDocument();
 });
