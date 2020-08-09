7a66280ebf2066bea4d920781013aeb26bea27dc
anyruizd <anyruizd@gmail.com>
2020-08-09 00:06:29 -0500

    Add toggle button styles

---
+++ src/components/ToggleButton/ToggleButton.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../../constants';
+
+const Toggle = styled.div`
+  align-items: flex-end;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  height: 28px;
+  justify-content: space-around;
+  width: 36px;
+  padding-right: 20px;
+
+  .line {
+    background-color: ${COLORS.darkElement};
+    border-radius: 2px;
+    height: 4px;
+    width: 80px;
+
+    &-top {
+      width: 36px;
+    }
+
+    &-mid {
+      width: 22px;
+    }
+
+    &-bottom {
+      width: 30px;
+    }
+  }
+`;
+
+export function ToggleButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Toggle
+      onClick={() => setIsOpen(!isOpen)}
+      className={`toggle-container ${isOpen ? 'menu-open' : 'menu-closed'}`}
+    >
+      <span className="line line-top"></span>
+      <span className="line line-mid"></span>
+      <span className="line line-bottom"></span>
+    </Toggle>
+  );
+}
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const COLORS = {
+  darkBackground: '#1c1627',
+  darkText: '#F0E6EF',
+  darkElement: '#F0A6CA',
+};
